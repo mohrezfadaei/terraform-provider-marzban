f8a94439d2c7c210e2846698c747fcd213b4c81d
Mohammad Reza Fadaei <mohrezfadaei@gmail.com>
2024-08-13 14:37:03 +0330

    feat(resource): add resource node implementation
    - Implement resource schema for 'marzban_node' with fields: name, address, port, api_port, usage_coefficient, and add_as_new_host
    - Add Create, Read, Update, and Delete (CRUD) operations for the 'marzban_node' resource
    - Handle API interactions for creating, reading, updating, and deleting nodes using the Marzban API
    - Define schema validation and default values for resource fields
    - Integrate with the provider and client to manage node resources effectively
    - Ensure proper handling of API responses and error cases

--- marzban/resource_node.go
+++ marzban/resource_node.go
@@ -2,6 +2,7 @@ package marzban
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -52,17 +53,72 @@ func resourceNode() *schema.Resource {
 }
 
 func resourceNodeCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	return nil
+	client := m.(*Client)
+	name := d.Get("name").(string)
+	address := d.Get("address").(string)
+	port := d.Get("port").(int)
+	apiPort := d.Get("api_port").(int)
+	usageCoefficient := d.Get("usage_coefficient").(float64)
+	addAsNewHost := d.Get("add_as_new_host").(bool)
+
+	nodeID, err := client.CreateNode(name, address, port, apiPort, usageCoefficient, addAsNewHost)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(fmt.Sprintf("%d", nodeID))
+	return resourceNodeRead(ctx, d, m)
 }
 
 func resourceNodeRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*Client)
+	NodeID := d.Id()
+
+	node, err := client.GetNode(NodeID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("name", node.Name)
+	d.Set("address", node.Address)
+	d.Set("port", node.Port)
+	d.Set("api_port", node.ApiPort)
+	d.Set("usage_coefficient", node.UsageCoefficient)
+	d.Set("add_as_new_host", node.AddAsNewHost)
+
 	return nil
 }
 
 func resourceNodeUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	return nil
+	client := m.(*Client)
+	nodeID := d.Id()
+
+	if d.HasChanges("name", "address", "port", "api_port", "usage_coefficient", "add_as_new_host") {
+		name := d.Get("name").(string)
+		address := d.Get("address").(string)
+		port := d.Get("port").(int)
+		apiPort := d.Get("api_port").(int)
+		usageCoefficient := d.Get("usage_coefficient").(float64)
+		addAsNewHost := d.Get("add_as_new_host").(bool)
+
+		err := client.UpdateNode(nodeID, name, address, port, apiPort, usageCoefficient, addAsNewHost)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	return resourceNodeRead(ctx, d, m)
 }
 
 func resourceNodeDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*Client)
+	nodeID := d.Id()
+
+	err := client.DeleteNode(nodeID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
 	return nil
 }
