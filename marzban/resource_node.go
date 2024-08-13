aba1577e2e0715577bf4906d34155772fd410898
Mohammad Reza Fadaei <mohrezfadaei@gmail.com>
2024-08-13 13:31:32 +0330

    feat(resource) add node resources

--- marzban/resource_node.go
+++ marzban/resource_node.go
@@ -1,7 +1,35 @@
 package marzban
 
-import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
 
 func resourceNode() *schema.Resource {
-	return &schema.Resource{}
+	return &schema.Resource{
+		CreateContext: resourceNodeCreate,
+		ReadContext:   resourceNodeRead,
+		UpdateContext: resourceNodeUpdate,
+		DeleteContext: resourceNodeDelete,
+
+		Schema: map[string]*schema.Schema{},
+	}
+}
+
+func resourceNodeCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceNodeRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceNodeUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceNodeDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
 }
